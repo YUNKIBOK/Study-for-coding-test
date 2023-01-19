74ad1717b4547953ffa41c42f32ce6167e8d81c9
YUNKIBOK <qhr6442@gmail.com>
2023-01-19 22:16:30 +0900

    2023-01-19

---
+++ java/baekjoon/2444.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		byte N = Byte.parseByte(br.readLine());
+		// up part
+		for (int i = 1; i <= N - 1; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		// middle part
+		for (int i = 0; i < 2 * N - 1; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
+
+		// down part
+		for (int i = N - 1; i >= 1; i--) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
